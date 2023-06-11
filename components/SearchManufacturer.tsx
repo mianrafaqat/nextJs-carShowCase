d72cd0cc92db39eb422e6da2ce159b82f6a6a8c9
Rafaqat Ali <83506643+mianrafaqat@users.noreply.github.com>
2023-06-11 15:28:08 +0500

    manufacturing

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { SearchManufacturerProps } from "@/types";
+import { Combobox, Transition } from "@headlessui/react";
+import Image from "next/image";
+import React, { Fragment, useState } from "react";
+
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+  return (
+    <div className=" search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="car"
+            />
+          </Combobox.Button>
+
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+
+          <Transition
+            as={Fragment}
+            Leave="transition ease-in duration-100"
+            LeaveFrom="opacity-100"
+            LeaveTo="opacity=0"
+            afterLeave={() => setQuery("")}
+          ></Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+};
+
+export default SearchManufacturer;
