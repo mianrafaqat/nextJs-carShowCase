f8806cc8e8a7304c000d0034986677f9e2063c95
Rafaqat Ali <83506643+mianrafaqat@users.noreply.github.com>
2023-06-11 14:21:52 +0500

    car catalouge

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomFilter = () => {
+  return <div>CustomeFilter</div>;
+};
+
+export default CustomFilter;
--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,7 @@ import Hero from "./Hero";
 import CustomeButton from "./CustomeButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
 
-export { Hero, CustomeButton, Navbar, Footer };
+export { Hero, CustomeButton, Navbar, Footer, SearchBar, CustomFilter };
